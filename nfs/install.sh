947e39139755df6d83d9b9cc0cd8ffb3366b8f00
Eric Kang <erickangatms@outlook.com>
2017-10-17 17:58:55 -0700

    .

--- nfs/install.sh
+++ nfs/install.sh
@@ -1,3 +1,14 @@
 
 
 apt-get install nfs-kernel-server
+
+
+# update /etc/exports
+/k8svolumes  *(rw,all_squash)
+
+sudo chown -R nobody:nogroup /k8svolumes
+sudo exportfs -a
+
+showmount -e kube
+
+sudo systemctl start nfs-kernel-server.service
