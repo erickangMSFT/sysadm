4204d08304df81db0edfdcb2c0bf2d2441407e69
Eric Kang <erickangatms@outlook.com>
2017-10-19 16:12:40 -0700

    .

--- nfs/install.sh
+++ nfs/install.sh
@@ -4,7 +4,7 @@ apt-get install nfs-kernel-server
 
 
 # update /etc/exports
-/k8svolumes  *(rw,all_squash)
+/k8svolumes  *(rw,async,no_subtree_check,no_root_squash)
 
 sudo chown -R nobody:nogroup /k8svolumes
 sudo exportfs -a
