0a2cb5ae48ebfdc2a87e96c6841a8fc48cd35806
Eric Kang <erickang@microsoft.com>
2017-11-22 09:34:39 -0800

    .

--- kvm/2-install-k8s-vm.sh
+++ kvm/2-install-k8s-vm.sh
@@ -5,20 +5,7 @@ apt-get -y install libosinfo-bin libguestfs-tools virt-top
 # create a storage pool
 mkdir -p /var/kvm/images 
 
-virt-install \
---name template \
---ram 4096 \
---disk path=/var/kvm/images/template.img,size=50 \
---vcpus 2 \
---os-type linux \
---os-variant ubuntu16.04 \
---network bridge=br0 \
---graphics none \
---console pty,target_type=serial \
---location 'http://us.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/' \
---extra-args 'console=ttyS0,115200n8 serial'
-
-#k8s-node
+#k8snode
 virt-install \
 --name k8snode \
 --ram 4096 \
---
+++ kvm/misc/vm-template.sh
@@ -0,0 +1,12 @@
+virt-install \
+--name template \
+--ram 4096 \
+--disk path=/var/kvm/images/template.img,size=50 \
+--vcpus 2 \
+--os-type linux \
+--os-variant ubuntu16.04 \
+--network bridge=br0 \
+--graphics none \
+--console pty,target_type=serial \
+--location 'http://us.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/' \
+--extra-args 'console=ttyS0,115200n8 serial'
