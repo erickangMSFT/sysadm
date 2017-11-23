1e6097b46052ca1213506d3eb1cc0b724a7ced12
Eric Kang <erickang@microsoft.com>
2017-11-23 06:26:21 -0800

    fixed flannel pod network issue

--- kubernetes/1-install-master.sh
+++ kubernetes/1-install-master.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sudo kubeadm init --pod-network-cidr=111.111.0.0/16 --network-plugin=cni #--token-ttl 0
+export CALICO_IPV4POOL_CIDR=172.16.0.0
+kubeadm init --pod-network-cidr=$CALICO_IPV4POOL_CIDR/16
+
+#sudo kubeadm init --pod-network-cidr=111.111.0.0/16 --network-plugin=cni #--token-ttl 0
 
 # Make sure save the output from this kubeadm init and follow the instruction.
--- kubernetes/3-install-addons.sh
+++ kubernetes/3-install-addons.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-#flannel network
-#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
+##Important to enable Kubernetes with kubeadm - do not use flannel pod network
+#wget https://docs.projectcalico.org/v2.5/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml -O calico.yaml
+#sed -i "s/192.168.0.0/'"$CALICO_IPV4POOL_CIDR"'/g" calico.yaml
 
-# calico network
-kubectl apply -f https://docs.projectcalico.org/v2.0/getting-started/kubernetes/installation/hosted/kubeadm/calico.yaml
+kubectl apply -f calico.yaml
+
 
 #optional
 kubectl apply -f https://git.io/weave-kube
 
 kubectl apply --namespace kube-system -f "https://cloud.weave.works/k8s/scope.yaml?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 kubectl port-forward -n kube-system "$(kubectl get -n kube-system pod --selector=weave-scope-component=app -o jsonpath='{.items..metadata.name}')" 4040
+
+
+#flannel network - do not use flannel with kubeadm
+#kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.0/Documentation/kube-flannel.yml
+
