be203038764e1625f10aaec01a7f05068f76c0ca
Eric Kang <erickang@microsoft.com>
2017-11-23 20:37:47 -0800

    .

--- kubernetes/1-install-master.sh
+++ kubernetes/1-install-master.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 export CALICO_IPV4POOL_CIDR=172.16.0.0
-kubeadm init --pod-network-cidr=$CALICO_IPV4POOL_CIDR/16
+export MASTER_NODE_IP=192.168.1.129
+kubeadm init --pod-network-cidr=$CALICO_IPV4POOL_CIDR/16 --apiserver-advertise-address=$MASTER_NODE_IP --network-plugin=cni
 
 #sudo kubeadm init --pod-network-cidr=111.111.0.0/16 --network-plugin=cni #--token-ttl 0
 
