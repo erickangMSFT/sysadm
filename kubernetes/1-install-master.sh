74c6f12371a375168d205dfcdd8e61f1bea3c643
Eric Kang <erickang@microsoft.com>
2017-11-24 09:30:54 -0800

    .

--- kubernetes/1-install-master.sh
+++ kubernetes/1-install-master.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-export POD_IPV4POOL_CIDR=172.16.0.0
+#export POD_IPV4POOL_CIDR=172.16.0.0
+export POD_IPV4POOL_CIDR=192.168.0.0
 export MASTER_NODE_IP=192.168.1.129
 kubeadm init --pod-network-cidr=$POD_IPV4POOL_CIDR/16 --apiserver-advertise-address=$MASTER_NODE_IP
 
