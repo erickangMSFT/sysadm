74a6c10d1d2a8726cf5134982eee31762afa9233
Eric Kang <erickang@microsoft.com>
2017-11-26 18:28:52 -0800

    .

--- kubernetes/1-install-master.sh
+++ kubernetes/1-install-master.sh
@@ -5,12 +5,11 @@ export POD_IPV4POOL_CIDR=10.244.0.0
 export MASTER_NODE_IP=192.168.1.137
 kubeadm init --pod-network-cidr=$POD_IPV4POOL_CIDR/16 --apiserver-advertise-address=$MASTER_NODE_IP
 
-#install weabe-net pod network
+#install weave-net pod network
 sysctl net.bridge.bridge-nf-call-iptables=1
 export kubever=$(kubectl version | base64 | tr -d '\n')
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$kubever"
 
-#sudo kubeadm init --pod-network-cidr=111.111.0.0/16 --network-plugin=cni #--token-ttl 0
 #kubectl apply -f https://docs.projectcalico.org/v2.6/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
 
 # Make sure save the output from this kubeadm init and follow the instruction.
--- kubernetes/2-install-agent.sh
+++ kubernetes/2-install-agent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #on each agent node,
+sysctl net.bridge.bridge-nf-call-iptables=1
 
 # run kubeadm --join commnad by pasting it from master's kubeadm --init command result.
