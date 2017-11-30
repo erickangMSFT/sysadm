0691fd2ff4026ac4f42a14569458e70a309a44f8
Eric Kang <erickang@microsoft.com>
2017-11-30 13:23:07 -0800

    .

--- kvm/2-install-k8s-vm.sh
+++ kvm/2-install-k8s-vm.sh
@@ -5,10 +5,10 @@ apt-get -y install libosinfo-bin libguestfs-tools virt-top
 # create a storage pool
 mkdir -p /var/kvm/images 
 
-#k8snode
+#k8smaster
 virt-install \
---name k8snode \
---ram 8192 \
+--name k8smaster \
+--ram 4096 \
 --disk path=/var/kvm/images/k8snode.img,size=50 \
 --vcpus 2 \
 --os-type linux \
@@ -22,10 +22,34 @@ virt-install \
 # During the installation do not enable SWAP. Kubernetes will not run if swap is enabled.
 
 # after installation, back to KVM host and shutdown the guest like following
-virsh shutdown k8snode 
+virsh shutdown k8smaster 
 
 # mount guest's disk and enable a service like following
-guestmount -d k8snode -i /mnt 
+guestmount -d k8smaster -i /mnt 
+ln -s /mnt/lib/systemd/system/getty@.service /mnt/etc/systemd/system/getty.target.wants/getty@ttyS0.service 
+umount /mnt
+
+#k8snode
+virt-install \
+--name k8sworker \
+--ram 12288 \
+--disk path=/var/kvm/images/k8snode.img,size=50 \
+--vcpus 2 \
+--os-type linux \
+--os-variant ubuntu16.04 \
+--network bridge=br0 \
+--graphics none \
+--console pty,target_type=serial \
+--location 'http://us.archive.ubuntu.com/ubuntu/dists/xenial/main/installer-amd64/' \
+--extra-args 'console=ttyS0,115200n8 serial'
+
+# During the installation do not enable SWAP. Kubernetes will not run if swap is enabled.
+
+# after installation, back to KVM host and shutdown the guest like following
+virsh shutdown k8sworker 
+
+# mount guest's disk and enable a service like following
+guestmount -d k8sworker -i /mnt 
 ln -s /mnt/lib/systemd/system/getty@.service /mnt/etc/systemd/system/getty.target.wants/getty@ttyS0.service 
 umount /mnt
 
