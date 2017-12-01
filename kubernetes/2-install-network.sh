a35963f029ece3ff87731311b2a6005c184be605
Eric Kang <erickang@microsoft.com>
2017-11-30 16:55:51 -0800

    .

--- kubernetes/2-install-network.sh
+++ kubernetes/2-install-network.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-#export POD_IPV4POOL_CIDR=172.16.0.0
-export POD_IPV4POOL_CIDR=10.244.0.0
-export MASTER_NODE_IP=10.135.9.19
-kubeadm init --pod-network-cidr=$POD_IPV4POOL_CIDR/16 --apiserver-advertise-address=$MASTER_NODE_IP > k8s.txt
-
 #install weave-net pod network
 
 export kubever=$(kubectl version | base64 | tr -d '\n')
